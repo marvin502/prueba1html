82087e8abc4bf5723e5b2f839021e6bcb604428b
marvin502 <marvinarnoldojime@gamil.com>
2022-07-25 22:16:12 -0600

    update

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Prueba de Funcionamiento de GITHUB</h1>
+    <p>
+        <h2>Haciendo Prueba</h2>
+    </p>
+    <p>
+        <a href="funcionamiento.html">Ir a Funcionamiento</a>
+    </p>
+</body>
+</html>
